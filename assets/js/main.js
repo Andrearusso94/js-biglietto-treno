71fcbb4f617b05486ee66f12147976c6bfa382fb
Andrearusso94 <andrea.r.4845@gmail.com>
2022-10-06 16:18:33 +0200

    add for donato

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,22 +10,25 @@ console.log(nKm);
 const price = nKm * 0.21;
 console.log(price);
 
-// sconti 
- const discountValueUnder = Math.round((price / 100)* 20) ;
- const discountValueOver = Math.round((price/100)*40) ;
-
+// sconti  
+ let finalPrice;
+ const discountValueUnder = Math.round((price / 100)* 20);
+ const discountValueOver = Math.round((price/100)* 40) ;
+ 
+ 
 
  //prezzi scontati
 if (age < 18) {
-
-   console.log(priceDiscount = price - discountValueUnder);
+ finalPrice = price - discountValueUnder;
 } else if (age > 65) {
-    console.log(priceDiscount = price - discountValueOver);
-} else {
-    console.log("Non si applica nessuno sconto");
+   finalPrice = price - discountValueOver;
+} else  {
+    finalPrice = price
+    console.log("Non si applica nessuno sconto", finalPrice);
 }
 
 
 
-let myMessage = `Ciao questo è il prezzo del tuo biglietto:€ ${priceDiscount}`
-document.getElementById("correct_price").innerHTML = myMessage
+let myMessage = `Ciao questo è il prezzo del tuo biglietto:€ ${finalPrice}`
+document.getElementById("correct_price").innerHTML = myMessage;
+
