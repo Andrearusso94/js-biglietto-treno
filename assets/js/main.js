8be39880bfbcb55e96b8a4b4aaf189e41475edbb
Andrearusso94 <andrea.r.4845@gmail.com>
2022-10-06 14:37:52 +0200

    add questions user

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+//età viaggiatore
+const age = Number(prompt('Quanti anni hai?'));
+console.log(age);
+
+//km da percorrere
+const nKm = Number(prompt('Quanti Km devi percorrere?'));
+console.log(nKm);
